2aa597a98b3532233e4d7ac101b23b20ef9f979e
lidachao111222 <chao.li@usask.ca>
2021-06-13 12:40:43 +0800

    fix the file name bug. And remove the login component.

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,86 +1,12 @@
-import React, { useState } from 'react';
-import {
-  Flex,
-  Box,
-  Heading,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  CircularProgress,
-  Text,
-  InputGroup,
-  InputRightElement,
-  Icon,
-} from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import ErrorMessage from '../common/ErrorMessage';
+import { Box } from '@chakra-ui/react';
+import React from 'react';
 
-export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-
+const SignIn = () => {
   return (
-    <Flex width="full" align="center" justifyContent="center">
-      <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
-        {isLoggedIn ? (
-          <Box textAlign="center">
-            <Text>{email} logged in!</Text>
-            <Button variantColor="orange" variant="outline" width="full" mt={4} onClick={() => setIsLoggedIn(false)}>
-              Sign out
-            </Button>
-          </Box>
-        ) : (
-          <>
-            <Box textAlign="center">
-              <Heading>Login</Heading>
-            </Box>
-            <Box my={4} textAlign="left">
-              <form
-              //   onSubmit={}
-              >
-                {error && <ErrorMessage message={error} />}
-                <FormControl isRequired>
-                  <FormLabel>Email</FormLabel>
-                  <Input
-                    type="email"
-                    placeholder="test@test.com"
-                    size="lg"
-                    onChange={(event) => setEmail(event.currentTarget.value)}
-                  />
-                </FormControl>
-                <FormControl isRequired mt={6}>
-                  <FormLabel>Password</FormLabel>
-                  <InputGroup>
-                    <Input
-                      type={showPassword ? 'text' : 'password'}
-                      placeholder="*******"
-                      size="lg"
-                      onChange={(event) => setPassword(event.currentTarget.value)}
-                    />
-                    <InputRightElement width="3rem">
-                      <Button
-                        h="1.5rem"
-                        size="sm"
-                        // onClick={}
-                      >
-                        {showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                </FormControl>
-                <Button variantColor="teal" variant="outline" type="submit" width="full" mt={4}>
-                  {isLoading ? <CircularProgress isIndeterminate size="24px" color="teal" /> : 'Sign In'}
-                </Button>
-              </form>
-            </Box>
-          </>
-        )}
-      </Box>
-    </Flex>
+    <Box>
+        signin
+    </Box>
   );
-}
+};
+
+export default SignIn;
