b24286551e9186dccd6fd6e0581c35181224203d
Will Wei <will.wei@usask.ca>
2021-06-13 13:19:21 -0600

    router

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import {  Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import { CircularProgress } from '@chakra-ui/react';
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const HomeLazy = React.lazy(() => import('../pages/Home') as never);
+const SignInLazy = React.lazy(() => import('../pages/SignIn') as never);
+const SignUpLazy = React.lazy(() => import('../pages/SignUp') as never);
 
 const BasicRoute = () => {
   return (
-  
+    <Suspense fallback={<CircularProgress isIndeterminate />}>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/" component={HomeLazy} />
+        <Route exact path="/signin" component={SignInLazy} />
+        <Route exact path="/signup" component={SignUpLazy} />
       </Switch>
- 
+    </Suspense>
   );
 };
 
