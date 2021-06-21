3edb0fa1e8e6b29a2ef7e5cf8ad9867c2fd06132
Will Wei <will.wei@usask.ca>
2021-06-20 20:39:45 -0600

    calendar

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,28 @@
 import { Box, Button } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import enUS from 'date-fns/locale/en-US';
 import { fetchUser, signOut } from '../../utils/api_handlers';
 import { User } from '../../utils/User';
+
+// import 'react-big-calendar/lib/sass/styles';
+import './HomeCalendar.css';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 const Home: React.FC = () => {
   const [user, setUser] = useState<User>();
@@ -11,10 +32,11 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <Box>
+    <Box height="500">
       Hello, {user?.nickname}!
       <br />
       <Button onClick={handleSignOut}>Sign Out</Button>
+      <Calendar localizer={localizer} events={[]} style={{ height: '500' }} />
     </Box>
   );
 };
