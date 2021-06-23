d5e95adc0bf5271bfa4a6383fbc9b53af52832e1
willwei954 <will.wei@usask.ca>
2021-06-23 08:23:55 -0600

    prettier

--- src/utils/api_handlers.ts
+++ src/utils/api_handlers.ts
@@ -1,56 +1,44 @@
 /* eslint-disable import/prefer-default-export */
 
-import ServerInfo from "./ServerInfo";
-import { UserSignInInfo, UserSignUpInfo } from "./User";
+import ServerInfo from './ServerInfo';
+import { UserSignInInfo, UserSignUpInfo } from './User';
 
 export async function signIn({ username, authenticationHash }: UserSignInInfo) {
-  return fetch(
-    `${ServerInfo.BASE_URL}/authentication/sign-in`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        authenticationHash,
-      }),
-      credentials: 'include',
+  return fetch(`${ServerInfo.BASE_URL}/authentication/sign-in`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({
+      username,
+      authenticationHash,
+    }),
+    credentials: 'include',
+  });
 }
 
 export async function signUp(payload: Partial<UserSignUpInfo>) {
-  return fetch(
-    `${ServerInfo.BASE_URL}/authentication/sign-up`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
+  return fetch(`${ServerInfo.BASE_URL}/authentication/sign-up`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify(payload),
+  });
 }
 
 export async function fetchUser() {
-  return fetch(
-    `${ServerInfo.BASE_URL}/user/fetch`,
-    {
-      method: 'GET',
-      credentials: 'include',
-    },
-  );
+  return fetch(`${ServerInfo.BASE_URL}/user/fetch`, {
+    method: 'GET',
+    credentials: 'include',
+  });
 }
 
 export async function signOut() {
-  return fetch(
-    `${ServerInfo.BASE_URL}/authentication/sign-out`,
-    {
-      method: 'POST',
-      credentials: 'include',
-    },
-  );
+  return fetch(`${ServerInfo.BASE_URL}/authentication/sign-out`, {
+    method: 'POST',
+    credentials: 'include',
+  });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
